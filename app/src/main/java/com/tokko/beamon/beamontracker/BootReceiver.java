179d12ff3e68089885b2458d8d307b32da93b4d9
tokko <andreg@kth.se>
2016-02-29 19:39:16 +0100

    bootreceiver

---
+++ app/src/main/java/com/tokko/beamon/beamontracker/BootReceiver.java
@@ -0,0 +1,15 @@
+package com.tokko.beamon.beamontracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    public BootReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, LocationService.class).setAction(LocationService.ACTION_REGISTER));
+    }
+}
